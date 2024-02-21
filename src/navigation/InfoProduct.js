77905a0326ea789ebfa8d81dc5322740c9f03ae8
Carlaurence <ingeniero.indbermeo@gmail.com>
2024-02-21 12:50:23 -0500

    cambio lijero modulo  InfoProduct

--- src/navigation/InfoProduct.js
+++ src/navigation/InfoProduct.js
@@ -1,3 +1,4 @@
+/** */
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams, Link, useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
